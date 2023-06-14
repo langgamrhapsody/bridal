ea49b42789f9d2b10147816ba12a74145cfb9be5
langgamrhapsody <66179653+langgamrhapsody@users.noreply.github.com>
2023-06-14 11:36:19 +0800

    first commit with navbar components

---
+++ components/navbar/navbar.php
@@ -0,0 +1,54 @@
+<nav class="navbar navbar-expand-lg navbar-dark custom-nav">
+    <div class="container navbar__custom">
+        <a class="navbar__brand" href="/">
+            <img class="navbar__brand-img" src="../../assets//img/logo__brand.png" alt="logo-brand">
+        </a>
+        <div class="offcanvas offcanvas-end offcanvas__custom " id="offcanvasRight"
+            aria-labelledby="offcanvasRightLabel">
+            <div class="offcanvas__header">
+                <button type="button" class="offcanvas__header-button btn-close " data-bs-dismiss="offcanvas"
+                    aria-label="Close"></button>
+            </div>
+            <ul class="navbar__nav">
+                <li class="navbar__nav-item">
+                    <a class="navbar__nav-item-link active " aria-current="page" href="/">Home</a>
+                </li>
+                <li class="navbar__nav-item">
+                    <a class="navbar__nav-item-link" href="/property.php">Our Story</a>
+                </li>
+                <li class="navbar__nav-item">
+                    <a class="navbar__nav-item-link" href="#">Services</a>
+                </li>
+                <li class="navbar__nav-item">
+                    <a class="navbar__nav-item-link ">Wedding Venues</a>
+                </li>
+                <li class="navbar__nav-item">
+                    <a class="navbar__nav-item-link ">Wedding Stories</a>
+                </li>
+                <li class="navbar__nav-item">
+                    <a class="navbar__nav-item-link" href="/contact.php">Contact</a>
+                </li>
+
+            </ul>
+        </div>
+        <div class="navbar__button-container">
+            <button class="btn navbar__toggler" type="button" data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                <svg xmlns="http://www.w3.org/2000/svg" class="navbar__toggler-icon" viewBox="0 0 448 512" width="20"
+                    height="20">
+                    <path fill="currentColor"
+                        d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+                </svg>
+            </button>
+            <div class="navbar__search">
+                <input class="navbar__search-input" type="text" placeholder="search...">
+                <button type="submit" class="btn navbar__search-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="16" height="16">
+                        <path fill="currentColor"
+                            d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                    </svg> </button>
+            </div>
+        </div>
+
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include_once './stylesheets/globalStylesheet.php'; ?>
+    <link href="./assets/css/pages/home.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <title>Resideo.</title>
+</head>
+
+<body>
+
+    <header>
+        <!-- Navbar -->
+        <?php include_once './components/navbar/navbar.php'; ?>
+    </header>
+    <main>
+
+
+    </main>
+    <footer>
+    </footer>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+    <script src="./assets/js/layout.js"></script>
+
+</body>
+
+</html>
